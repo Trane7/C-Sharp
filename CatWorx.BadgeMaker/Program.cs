507eb91a5775066f6756f7afe2270032cd604724
Dee <dgrowette@gmail.com>
2022-05-27 11:53:32 -0700

    trying out arrays

--- CatWorx.BadgeMaker/Program.cs
+++ CatWorx.BadgeMaker/Program.cs
@@ -7,22 +7,12 @@ namespace CatWorx.BadgeMaker // This is usually the companys name to help show u
     {
         static void Main(string[] args) // Main() is the entry point of the app
         {
-            Dictionary<string, int> myScoreBoard = new Dictionary<string, int>(){
-                { "firstInning", 10 },
-                { "secondInning", 20 },
-                { "thirdInning", 30 },
-                { "fourthInning", 40 },
-                { "fifthInning", 50 }
-            };
-            Console.WriteLine("----------------");
-            Console.WriteLine("  Scoreboard");
-            Console.WriteLine("----------------");
-            Console.WriteLine("Inning |  Score");
-            Console.WriteLine("   1   |    {0}", myScoreBoard["firstInning"]);
-            Console.WriteLine("   2   |    {0}", myScoreBoard["secondInning"]);
-            Console.WriteLine("   3   |    {0}", myScoreBoard["thirdInning"]);
-            Console.WriteLine("   4   |    {0}", myScoreBoard["fourthInning"]);
-            Console.WriteLine("   5   |    {0}", myScoreBoard["fifthInning"]);
+            string[] favFoods = new string[3]{ "pizza", "doughnuts", "icream" };
+            string firstFood =  favFoods[0];
+            string secondFood = favFoods[1];
+            string thirdFood = favFoods[2];
+            Console.WriteLine("I like {0}, {1}, {2}", firstFood, secondFood, thirdFood);
+
         }
     }
 }
